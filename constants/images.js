7394426f71e2ba583daa20fd8f79d43b63784a1c
Mahamud Aden <mahamudadn@gmail.com>
2024-10-18 17:22:25 -0500

    added assets and constant folders to use in the project

---
+++ constants/images.js
@@ -0,0 +1,7 @@
+import profile from "../assets/images/profile.png";
+import thumbnail from "../assets/images/thumbnail.png";
+import cards from "../assets/images/cards.png";
+import path from "../assets/images/path.png";
+import logo from "../assets/images/logo.png";
+
+export default { profile, thumbnail, cards, path, logo,   };
