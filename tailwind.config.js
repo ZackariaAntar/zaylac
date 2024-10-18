81c72a528b16e6d413ad47521f906df1d902999f
Mahamud Aden <mahamudadn@gmail.com>
2024-10-18 14:56:20 -0500

    set up tailwind css and deleted the content in my index.jsx

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  ccontent: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
