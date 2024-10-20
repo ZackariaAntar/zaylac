1e03cc98dde226ea46bdaa97974959d734d14111
Mahamud Aden <mahamudadn@gmail.com>
2024-10-20 01:30:50 -0500

    craeted the auth-screen and the tabs for the app

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-primary">
-      <Text className="text-2xl color-white font-pblack">Kaayo</Text>
+      <Text className="text-2xl color-white font-pbold">Kaayo</Text>
       <StatusBar/>
       
     </View>
