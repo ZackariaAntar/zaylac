c93fbed369cb5d21a8b0b385a704d97b941e6a07
Mahamud Aden <mahamudadn@gmail.com>
2024-10-20 00:29:26 -0500

    added default colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#161622",
+        primary: "#006B61",
         secondary: {
           DEFAULT: "#E8B931",
           100: "#FF9001",
