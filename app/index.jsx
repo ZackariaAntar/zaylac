b073fe3a9b0b20d48b22472d3063b5253d5f37b9
Mahamud Aden <mahamudadn@gmail.com>
2024-10-20 14:23:43 -0500

    imported Link amd made a link that goes to the Home

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,6 @@
 import {  View, Text } from 'react-native'
 import { StatusBar } from "expo-status-bar";
-import React from 'react'
-
+import { Link } from "expo-router";
 export default function App ()  {
 
 return (
@@ -9,6 +8,7 @@ return (
     <View className="flex-1 items-center justify-center bg-primary">
         <Text className="text-5xl font-pbold">Kaayo</Text>
         <StatusBar style="uato"/>
+        <Link href="/home" style={{color:'blue'}}> Go Home</Link>
     </View>
 )
 }
