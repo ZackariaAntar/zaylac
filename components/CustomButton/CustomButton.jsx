b1e2b88a05247122360cadf98588f125ad3bc412
Mahamud Aden <mahamudadn@gmail.com>
2024-10-21 10:13:45 -0500

    added a touchable opacity to the custombutton

--- components/CustomButton/CustomButton.jsx
+++ components/CustomButton/CustomButton.jsx
@@ -1,11 +1,11 @@
-import { View, Text } from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
 import React from 'react'
 
 const CustomButton = () => {
     return (
-        <View>
+        <TouchableOpacity>
         <Text>CustomButton</Text>
-        </View>
+        </TouchableOpacity>
     )
     }
 
