e340554465cbc272e18a2ea22200b73eba8676cb
Mahamud Aden <mahamudadn@gmail.com>
2024-10-23 23:36:14 -0500

    added a text and passed the styles and values

--- components/FormField/FormField.jsx
+++ components/FormField/FormField.jsx
@@ -5,6 +5,18 @@ const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ..
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-base text-white-200 font-pmedium">{title}</Text>
+      <View className="border-2 border-black-200 w-full h-16
+                         px-4 bg-white-100 rounded-2xl focus:border-secondary
+                        items-center
+      ">
+        <Text className="flex-1 text-white-200 font-psemibold text-base"
+            value= {value}
+            placeholder={placeholder}
+            placeholderTextColor="#7b7b8b"
+            onChangeText={handleChangeText}
+
+        />
+      </View>
     </View>
   )
 }
