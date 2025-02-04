5ac16c443e5ecbb8ed733c2e80866d7061cd4971
Zackaria Antar <antarzack@gmail.com>
2025-02-04 17:45:09 -0600

    created some add group logic, still very raw

--- redux/rootReducer.jsx
+++ redux/rootReducer.jsx
@@ -1,9 +1,11 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from './slices/authSlice'
 import user from './slices/userSlice'
+import group from './slices/groupSlice'
 const rootReducer = combineReducers({
     auth,
     user,
+    group,
 
 
 })
